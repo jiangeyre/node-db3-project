1764f9ad23de83d1143e412b68487286ab43a8ea
Lexie Jiang <LexieJiang@Lexies-MacBook-Pro.local>
2020-01-22 14:21:20 -0800

    Commit 4: some schemes done

--- schemes/scheme-model.js
+++ schemes/scheme-model.js
@@ -1,7 +1,22 @@
 const db = require('../db-config.js');
 
 module.exports = {
-
+    find,
+    findById,
+    findSteps,
+    add,
+    update,
+    remove
 };
 
-function 
+function find() {
+    return db('schemes')
+        .select();
+};
+
+function findById(id) {
+    return db('schemes')
+        .where({ id })
+        .first();
+};
+
